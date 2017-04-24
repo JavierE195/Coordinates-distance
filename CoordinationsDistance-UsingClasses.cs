619588e5a3faf334c35d14b7a83e85db91408127
JavierE195 <ellerbrockjavier@hotmail.com>
2017-04-24 07:20:47 -0500

    Add files via upload

---
+++ CoordinationsDistance-UsingClasses.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication35
+{
+    class Point
+    {
+        public double xCoordinate;
+        public double yCoordinate;
+        private double _distance;
+
+        public Point()
+        {
+            xCoordinate = 0;
+            yCoordinate = 0;
+        }
+
+        public Point(double _xCoordinate, double _yCoordinate)
+        {
+            xCoordinate = _xCoordinate;
+            yCoordinate = _yCoordinate;
+        }
+        
+        public void Distance()
+        {
+            _distance = Math.Sqrt((xCoordinate * xCoordinate) + (yCoordinate * yCoordinate));
+        }
+
+        public double GetDistance()
+        {
+            return _distance;
+        }
+    }
+}
